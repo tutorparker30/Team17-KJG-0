783a87d3b6a4c57181e7888ae831ba7d6c28684d
(KimJunGi. wnsrlrla0) <wnsrlrla0@gmail.com>
2025-02-06 10:50:21 +0900

    기존 프로젝트 파일들 삭제.

--- Source/Sparta_v01/Sparta_v01.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Sparta_v01.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Sparta_v01, "Sparta_v01" );
